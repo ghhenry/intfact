6fa7bd17e66a1174ee14e4e7d9e3bab4ae5d9f13
Heinrich Nirschl <heinrich.nirschl@gmail.com>
2022-06-29 07:42:32 +0200

    add test candidate

--- rho_test.go
+++ rho_test.go
@@ -11,9 +11,12 @@ import (
 func TestRho(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
 	defer cancel()
+	// n=2^2^7+1
 	// takes 1227.18s in original version
 	// takes 731.99s in optimized version
 	//n, _ := new(big.Int).SetString("340282366920938463463374607431768211457", 10)
+	// n=2^67-1
+	//n, _ := new(big.Int).SetString("147573952589676412927", 10)
 	n, _ := new(big.Int).SetString("43217358712783469", 10)
 	d, err := Rho(ctx, n)
 	fmt.Printf("d=%v, e=%v\n", d, err)
